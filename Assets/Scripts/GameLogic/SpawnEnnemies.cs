ec3033db4c7629516e653a195c7a23fbd804d556
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-01-25 00:05:05 +0100

    ennemies spawning randomly quicker as time passes

--- Assets/Scripts/GameLogic/SpawnEnnemies.cs
+++ Assets/Scripts/GameLogic/SpawnEnnemies.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using UnityEngine;
+using Assets.Scripts.GameLogic;
 
 public class SpawnEnnemies : MonoBehaviour
 {
     [SerializeField] Object triangle;
 
-    readonly float xRadius = 8.9f;
-    readonly float yRadius = 5.0f;
+    readonly float xRadius = 9.5f;
+    readonly float yRadius = 5.5f;
     readonly float minSpawnInterval = 0.5f;
 
     float spawnInterval;
@@ -20,10 +21,14 @@ public class SpawnEnnemies : MonoBehaviour
 
     IEnumerator SpawnEnnemy()
     {
-        Instantiate(triangle, new Vector3(xRadius, yRadius, 0.0f), Quaternion.identity);
+        while (true)
+        {
+            Vector3 position = Helpers.GetRandomPositionOutside(xRadius, yRadius);
+            Instantiate(triangle, position, Quaternion.identity);
 
-        yield return new WaitForSeconds(spawnInterval);
+            yield return new WaitForSeconds(spawnInterval);
 
-        spawnInterval = Mathf.Max(spawnInterval - spawnInterval / 10, minSpawnInterval);
+            spawnInterval = Mathf.Max(spawnInterval - spawnInterval / 10, minSpawnInterval);
+        }
     }
 }
---
+++ Assets/Scripts/GameLogic/utils.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+
+namespace Assets.Scripts.GameLogic
+{
+  public class Helpers
+  {
+    public static Vector3 GetRandomPositionOutside(float xRadius, float yRadius)
+    {
+      int cases = Mathf.FloorToInt(Random.Range(0, 4));
+
+      Vector3 position = cases switch
+      {
+        // appears left
+        0 => new Vector3(-xRadius, Random.Range(-yRadius, yRadius), 0.0f),
+        // appears right
+        1 => new Vector3(xRadius, Random.Range(-yRadius, yRadius), 0.0f),
+        // appears bottom
+        2 => new Vector3(Random.Range(-xRadius, xRadius), -yRadius, 0.0f),
+        // appears top
+        3 => new Vector3(Random.Range(-xRadius, xRadius), yRadius, 0.0f),
+        _ => new Vector3(xRadius, yRadius, 0.0f),
+      };
+      return position;
+    }
+  }
+}
