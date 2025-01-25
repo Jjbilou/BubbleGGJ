dd1f7adb228fd97a09c7e2f86e3d2c4de800884a
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-01-25 20:12:30 +0100

    fix score and money not reseting

--- Assets/Scripts/GameLogic/Init.cs
+++ Assets/Scripts/GameLogic/Init.cs
@@ -16,14 +16,24 @@ public class Init : MonoBehaviour
 
     void Start()
     {
+        GameData.money = 0;
+        GameData.score = 0;
+        GameData.sawDuration = 0.0f;
+
         scoreCoroutine = StartCoroutine(AddScore());
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
+        // if (Input.GetKeyDown(KeyCode.Tab))
+        // {
+        //     GameData.isShopOpen = !GameData.isShopOpen;
+        // }
+
+        if (Input.GetKeyDown(KeyCode.A) && GameData.money >= 10)
         {
-            GameData.isShopOpen = !GameData.isShopOpen;
+            GameData.money -= 10;
+            GameData.sawDuration += 5.0f;
         }
     }
 
