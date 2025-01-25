2e112935689edeca9094fffb32189ed3fbc45b48
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-01-25 19:57:48 +0100

    made seasons change

---
+++ Assets/Scripts/ChangeSeason.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSeason : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] tileSprites;
+
+    [SerializeField]
+    float seasonDuration = 30.0f;
+
+    SpriteRenderer[] spriteRenderers;
+    int nbTilesBySeason;
+
+    void Start()
+    {
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        nbTilesBySeason = tileSprites.Length / 4;
+
+        StartCoroutine(Coroutine());
+    }
+
+    IEnumerator Coroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(seasonDuration);
+
+            for (int i = 0; i < spriteRenderers.Length; i++)
+            {
+                int index = Array.IndexOf(tileSprites, spriteRenderers[i].sprite);
+
+                spriteRenderers[i].sprite = tileSprites[
+                    (index + nbTilesBySeason) % tileSprites.Length
+                ];
+
+                if (i % 10 == 0)
+                {
+                    yield return null;
+                }
+            }
+        }
+    }
+}
