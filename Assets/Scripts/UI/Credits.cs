a54afe0015cd38b5127b0ea8f21ac6fcae8350c1
Nicolas <nico.dobosz@gmail.com>
2025-01-26 14:07:27 +0100

    credits

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -8,6 +8,7 @@ public class Credits : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Destroy(GameObject.Find("BGM"));
         StartCoroutine(DisplayCredits());
     }
 
@@ -15,9 +16,9 @@ public class Credits : MonoBehaviour
     {
         yield return new WaitForSeconds(3.0f);
 
-        transform.DOMoveY(10.8f * Screen.height, 60.0f).SetEase(Ease.Linear);
+        transform.DOMoveY(11300f, 60.0f).SetEase(Ease.Linear);
 
-        yield return new WaitForSeconds(63.0f);
+        yield return new WaitForSeconds(65.0f);
 
         SceneManager.LoadScene("MainMenu");
     }
