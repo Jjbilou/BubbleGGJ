b5d4bca3bb0a809280c6e610a34c8edffc9e9c33
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-01-26 12:15:10 +0100

    merge

--- Assets/Scripts/GameLogic/GameData.cs
+++ Assets/Scripts/GameLogic/GameData.cs
@@ -5,4 +5,7 @@ public class GameData : MonoBehaviour
     public static int money = 0;
     public static int score = 0;
     public static bool usePowerups = true;
+
+    public static int enemyKilled = 0;
+    public static int enemyKilledByP2 = 0;
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    KeyCode keyUp;
+
+    [SerializeField]
+    KeyCode keyLeft;
+
+    [SerializeField]
+    KeyCode keyDown;
+
+    [SerializeField]
+    KeyCode keyRight;
+
     public float speed = 300.0f;
 
     Rigidbody2D player;
@@ -13,7 +25,20 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        Vector2 movement = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        float xInput = 0.0f;
+        float yInput = 0.0f;
+
+        if (Input.GetKey(keyRight))
+            xInput += 1.0f;
+        if (Input.GetKey(keyLeft))
+            xInput -= 1.0f;
+
+        if (Input.GetKey(keyUp))
+            yInput += 1.0f;
+        if (Input.GetKey(keyDown))
+            yInput -= 1.0f;
+
+        Vector2 movement = new(xInput, yInput);
         movement.Normalize();
 
         player.velocity = speed * Time.deltaTime * movement;
